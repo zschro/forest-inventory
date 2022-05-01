a8bb8b2b8f546b410be1a80451fef87cfb929467
zack <zack@roofsnap.com>
2022-05-01 15:35:44 -0400

    material ui and some initial components

--- forest-inventory-react/src/App.js
+++ forest-inventory-react/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import ForestCard from './ForestCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stack direction="row" spacing={2}>
+        <ForestCard />
+        <ForestCard />
+        <ForestCard />
+        <ForestCard />
+        <ForestCard />
+        <ForestCard />
+      </Stack>
+      <Pagination count={10} />
     </div>
   );
 }
